6b0cdf1d84993fd9108ba1abcfd5961cf02c8c29
giancuffaro <giancuffaro230@gmail.com>
2026-08-28 23:52:50 -0400

    auto: MarketSniperRatchet.cs, MarketSniperTrend.cs (2026-08-28 23:52)

--- ninjatrader/MarketSniperRatchet.cs
+++ ninjatrader/MarketSniperRatchet.cs
@@ -1,10 +1,25 @@
 #region Using declarations
 using System;
+using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 using System.Windows.Media;
+using System.Xml.Serialization;
+using System.IO;
 using NinjaTrader.Cbi;
+using NinjaTrader.Gui;
+using NinjaTrader.Gui.Chart;
+using NinjaTrader.Gui.SuperDom;
+using NinjaTrader.Gui.Tools;
 using NinjaTrader.Data;
 using NinjaTrader.NinjaScript;
+using NinjaTrader.Core.FloatingPoint;
+using NinjaTrader.NinjaScript.DrawingTools;
 using NinjaTrader.NinjaScript.Strategies;
 #endregion
 
--- ninjatrader/MarketSniperTrend.cs
+++ ninjatrader/MarketSniperTrend.cs
@@ -1,17 +1,24 @@
 #region Using declarations
 using System;
-using System.IO;
-using System.Linq;
-using System.Text;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
 using System.Windows.Media;
+using System.Xml.Serialization;
+using System.IO;
 using NinjaTrader.Cbi;
 using NinjaTrader.Gui;
 using NinjaTrader.Gui.Chart;
+using NinjaTrader.Gui.SuperDom;
+using NinjaTrader.Gui.Tools;
 using NinjaTrader.Data;
 using NinjaTrader.NinjaScript;
+using NinjaTrader.Core.FloatingPoint;
 using NinjaTrader.NinjaScript.DrawingTools;
 #endregion
 
